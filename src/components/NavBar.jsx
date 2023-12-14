7588b938e2494c957f3af22be8b8dde7fff527be
Yhago Felipe <yhago.felipe.teles@gmail.com>
2023-12-14 16:35:12 -0300

    Css Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom"
-import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import "./NavBar.css";
 
-export default function NavBar(){
-    return(
-        <nav id="navbar">
-        <h2><Link to="/"><BiCameraMovie/>FilmotecaPlusLib</Link></h2>
-        <form>
-            <input type="text" placeholder="Procurar um filme..."/>
-            <button type="submit"><BiSearchAlt2/></button>
-        </form>
-      </nav>
-    )
+export default function NavBar() {
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie />
+          FilmotecaPlusLib
+        </Link>
+      </h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Procurar um filme..."
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
 }
